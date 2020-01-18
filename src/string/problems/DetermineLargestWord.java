3036d1ddfc7218774e9276cbec6086c919a685a7
dspjunaidkhalid <dspjunaidkhalid@gmail.com>
2020-01-18 15:33:43 -0500

    First commit

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -1,26 +1,31 @@
 package string.problems;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+
 
 public class DetermineLargestWord {
-    public static void main(String[] args) {
+
         /*
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-        String s="Human brain is a biological learning machine";
-        Map<Integer, String> wordNLength = findTheLargestWord(s);
-        //implement
 
+        static int LongestWordLength(String str)
+        {
+            String[] words = str.split(" ");
+            int length = 0;
+
+            for(String word:words){
+                if(length < word.length()){
+                    length = word.length();
+                }
+            }
+            return length;
+        }
+
+        public static void main(String args[])
+        {
+            String str ="Human brain is a biological learning machine";
+
+            System.out.println(LongestWordLength(str));
+        }
     }
-
-    public static Map<Integer, String> findTheLargestWord(String wordGiven){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
-        //implement
-
-        return map;
-    }
-}
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -9,8 +9,29 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        String string = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        int count;
 
+
+        string = string.toLowerCase();
+
+
+        String words[] = string.split(" ");
+
+        System.out.println("Duplicate words in a given string : ");
+        for(int i = 0; i < words.length; i++) {
+            count = 1;
+            for(int j = i+1; j < words.length; j++) {
+                if(words[i].equals(words[j])) {
+                    count++;
+
+                    words[j] = "0";
+                }
+            }
+
+
+            if(count > 1 && words[i] != "0")
+                System.out.println(words[i]);
+        }
     }
-
 }
